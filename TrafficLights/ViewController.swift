5fbb3676f69f9899c320b1002e563101120b1b8a
NosovaDaria <kuleshovadasha@gmail.com>
2021-12-08 21:42:52 +0300

    Bug fixes

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  //  MARK: - IB Outlets
   @IBOutlet var redLightView: UIView!
   @IBOutlet var yellowLightView: UIView!
   @IBOutlet var greenLightView: UIView!
+  
   @IBOutlet var startButton: UIButton!
   
+  // MARK: - Life Cycles Methods
   override func viewDidLoad() {
     super.viewDidLoad()
     redLightView.alpha = 0.3
     yellowLightView.alpha = 0.3
     greenLightView.alpha = 0.3
-    redLightView.layer.cornerRadius = redLightView.frame.size.width / 2
-    yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
-    greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
+
   }
   
+  override func viewDidAppear(_ animated: Bool) {
+    redLightView.layer.cornerRadius = redLightView.frame.size.height / 2
+    greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+    yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+  }
+    
+  // MARK: - IB Actions
   @IBAction func startPressed(_ sender: UIButton) {
+
     if redLightView.alpha < 1 && yellowLightView.alpha < 1 {
       greenLightView.alpha = 0.3
       redLightView.alpha = 1
