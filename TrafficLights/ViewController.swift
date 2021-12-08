aee3ba7b7070993f6aa9feaae336ffff2abdca09
NosovaDaria <kuleshovadasha@gmail.com>
2021-12-08 21:05:02 +0300

    Setup ViewController, LaunchScreen and Main

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet var redLightView: UIView!
+  @IBOutlet var yellowLightView: UIView!
+  @IBOutlet var greenLightView: UIView!
+  @IBOutlet var startButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    redLightView.alpha = 0.3
+    yellowLightView.alpha = 0.3
+    greenLightView.alpha = 0.3
+    redLightView.layer.cornerRadius = redLightView.frame.size.width / 2
+    yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
+    greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
   }
-
+  
+  @IBAction func startPressed(_ sender: UIButton) {
+    if redLightView.alpha < 1 && yellowLightView.alpha < 1 {
+      greenLightView.alpha = 0.3
+      redLightView.alpha = 1
+      startButton.setTitle("NEXT", for: .normal)
+    } else if redLightView.alpha == 1 {
+      redLightView.alpha = 0.3
+      yellowLightView.alpha = 1
+    } else if yellowLightView.alpha == 1 {
+      yellowLightView.alpha = 0.3
+      greenLightView.alpha = 1
+    }
+  }
 
 }
 
